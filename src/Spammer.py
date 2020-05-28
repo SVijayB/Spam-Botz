e81800390c7858744e690d9edd90bce912b57689
vijay <vijaybalajitheboss@gmail.com>
2020-05-28 19:23:39 +0530

    Added Delay After spamming instead of before

--- src/Spammer.py
+++ src/Spammer.py
@@ -3,9 +3,9 @@ import time
 
 keyboard = Controller()
 def spammer(message,sleep):
-    time.sleep(sleep)
     for unit in message:
         keyboard.press(unit)
         keyboard.release(unit)
     keyboard.press(Key.enter)
     keyboard.release(Key.enter)
+    time.sleep(sleep)
