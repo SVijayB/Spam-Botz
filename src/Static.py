b84c6f94f16437705ae30abe9ef671c20a8089d5
vijay <vijaybalajitheboss@gmail.com>
2020-05-28 14:52:20 +0530

    Made static independent of main

--- src/Static.py
+++ src/Static.py
@@ -1,3 +1,9 @@
+import time
+from Spammer import *
+
 def static():
     message = input("Enter the String you want to spam : ")
-    return(message)
+    count = int(input("Enter the number of times you want to spam the message : "))
+    print("Open Your Social Media Platform and select your text box")
+    time.sleep(5)
+    spammer(count,message)
--- src/main.py
+++ src/main.py
@@ -1,20 +1,20 @@
-from Spammer import *
 from Static import *
-import time
 
-choice = int(input("""How would you like to spam the user ? 
-1) Static Message
-2) Random Words From A Dictionary
-3) Series of Sequential Numbers
-4) Random Numbers\n>"""))
+if __name__ == "__main__":
+    choice = int(input("""How would you like to spam the user ? 
+    1) Static Message
+    2) Random Words From A Dictionary
+    3) Series of Sequential Numbers
+    4) Random Numbers\n> """))
 
-while(choice<1 or choice>4):
-    print("ERROR: ENTER VALUES BETWEEN 1 AND 4 ONLY")
-    choice = int(input(">"))
-if(choice==1):
-    message = static()
-
-count = int(input("Enter the number of times you want to spam the message : "))
-print("Open Your Social Media Platform and select your text box")
-time.sleep(5)
-spammer(count,message)
+    while(choice<1 or choice>4):
+        print("ERROR: ENTER VALUES BETWEEN 1 AND 4 ONLY")
+        choice = int(input("> "))
+    if(choice==1):
+        static()
+    if(choice==2):
+        random()
+    if(choice==3):
+        numbers()
+    if(choice==4):
+        randomnum()
