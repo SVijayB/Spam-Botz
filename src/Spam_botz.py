49cd00bf67ca96566302c119f99f4a7f165b63e5
vijay <vijaybalajitheboss@gmail.com>
2020-05-31 00:33:45 +0530

    Prevented program from closing after completion

--- src/Spam_botz.py
+++ src/Spam_botz.py
@@ -10,30 +10,48 @@ if __name__ == "__main__":
     data = open("../version.txt" , "r").read()
     print("Spam-Botz | " + data)
     time.sleep(1)
-    print("""How would you like to spam ? 
-    1) Static Message
-    2) Random Words From A Dictionary
-    3) Series of Sequential Numbers
-    4) Random Numbers
-    5) Rage Spam
-    6) Sentence breaker(into words).""")
-    choice = 0
-    while(choice<1 or choice>6):
-        try:
-            choice = int(input("> "))
-            if(choice<1 or choice>6):
-                raise ValueError
-        except ValueError:
-            print("ERROR : INVALID NUMBER")
-    if(choice==1):
-        static()
-    if(choice==2):
-        randwords()
-    if(choice==3):
-        numbers()
-    if(choice==4):
-        randomnum()
-    if(choice==5):
-        rage()
-    if(choice==6):
-        sentencebreaker()
+    while(True):
+        print("""What would you like to do ? 
+        1) Static Message Spam
+        2) Spam Random Words From A Dictionary
+        3) Spam A Series of Sequential Numbers
+        4) Spam Random Large Numbers
+        5) Rage Spam
+        6) Sentence breaker(into words) Spam
+        7) EXIT""")
+        choice = 0
+        while(choice<1 or choice>7):
+            try:
+                choice = int(input("> "))
+                if(choice<1 or choice>7):
+                    raise ValueError
+            except ValueError:
+                print("ERROR : INVALID NUMBER")
+        if(choice==1):
+            static()
+        if(choice==2):
+            randwords()
+        if(choice==3):
+            numbers()
+        if(choice==4):
+            randomnum()
+        if(choice==5):
+            rage()
+        if(choice==6):
+            sentencebreaker()
+        if(choice==7):
+            print("-----x Thank You For Using Spam-Botz x-----")
+            input("Press any key to exit ")
+            break
+        print("\nSuccessfully Completed Spamming")
+        ans = input("Do you want to continue your adventure on spamming? (Yes/No)\n> ")
+        if(ans=="Yes" or ans=="yes" or ans=="y" or ans=="Y"):
+            print("\nJourney Continues...\n")
+        elif(ans=="no" or ans=="No" or ans=="N" or ans=="n"):
+            print("-----x Thank You For Using Spam-Botz x-----")
+            input("Press any key to exit ")
+            break
+        else:
+            print("ERROR : INVALID INPUT")
+            input("Press any key to exit ")
+            break
